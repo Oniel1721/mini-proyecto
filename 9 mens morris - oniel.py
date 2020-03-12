7574fcbafddcb815ec7546afb8dc1db41df38488
Oniel_Santos <Oniel@gmail.com>
2020-03-12 09:45:32 -0300

    primer commit de mini proyecto

---
+++ 9 mens morris - oniel.py
@@ -0,0 +1,114 @@
+# def create_board():
+#   return ['*--*--*',
+#           '|*-*-*|',
+#           '||***||',
+#           '*** ***',
+#           '||***||',
+#           '|*-*-*|',
+#           '*--*--*']
+
+def create_board():
+  return [['*','-','-','*','-','-','*'],
+          ['|','*','-','*','-','*','|'],
+          ['|','|','*','*','*','|','|'],
+          ['*','*','*',' ','*','*','*'],
+          ['|','|','*','*','*','|','|'],
+          ['|','*','-','*','-','*','|'],
+          ['*','-','-','*','-','-','*']]
+
+def print_board(board):
+  for x in board:
+    print (x)
+    
+def fichas(board):
+  num_white = 0
+  num_black = 0
+  for i in board:
+    num_black += i.count("☻")
+    num_white += i.count("☺")
+  num_white = 3 - num_white
+  num_black = 3 - num_black
+  if num_black + num_white == 0:
+    return False
+  return [(str(num_white)+" "+"☺" * num_white),(str(num_black)+" "+"☻" * num_black)]
+  
+
+def not_valid(board,fil,col):
+  return fil + col > 12 or board[fil][col] != "*"
+
+def seleccionar(board,turno):
+  fil = int(input("Indique fila: "))
+  col = int(input("Indique columna: "))
+  return fil,col
+
+def tipo_ficha(turno):
+  if turno == 1:
+    return "☺",2
+  return "☻",1
+
+def goteo(board,turno):
+  fil,col = seleccionar(board,turno)
+  if not_valid(board,fil,col):
+    print('Ups la coordenada es incorrecta.')
+    print('Intentalo de nuevo.')
+    return goteo(board,turno)
+  ficha,turno = tipo_ficha(turno)
+  board [fil][col] = ficha
+  return board,turno
+
+
+def horizontal_moves(board,fil,col):
+  if fil < 3:
+    move = (6-fil) - 3
+  elif fil > 3:
+    move = fil - 3
+  else:
+    move = 1
+  if col-move >= 0 and col+move <= 6:
+    print('+En ambos')
+    if board[fil][col-move] != '*' and board[fil][col+move] != '*':
+      print('!en ambos')
+      return False
+  if col+move > 6:
+    print ('+en solo izquierda')
+    if board[fil][col-move] != '*':
+      print ('!en solo izquierda')
+      return False
+  if col-move < 0:
+    print ('+en solo derecha')
+    if board[fil][col+move] != '*':
+      print ('!en solo derecha')
+      return False
+  return True
+
+def vertical_moves(board,fil,col):
+  return True
+def diagonal_moves(board,fil,col):
+  return True
+
+
+
+def dezlice(board,turno):
+  fil,col = seleccionar(board,turno)
+  if horizontal_moves(board,fil,col):
+    board [fil][col] = 'X'
+  else:
+    print('No tiene movimientos')
+    return dezlice(board,turno)
+  return board,turno
+
+def game_loop():
+  board = create_board()
+  turno = 1
+  etapa = 1
+  while turno == 1 or turno == 2:
+    print_board(board)
+    if etapa == 1:
+      print (fichas(board))
+      board,turno = goteo(board,turno)
+      if fichas(board) == False:
+        etapa = 2
+      continue
+    board,turno = dezlice(board,turno)
+
+game_loop()
