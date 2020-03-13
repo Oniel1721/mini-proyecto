93184dfc400c1932aa186a9a7f33eaed6deb51a5
Oniel_Santos <Oniel@gmail.com>
2020-03-13 20:33:10 -0300

    Ya se detecta los molinos correctamente,aun falta que los detecte en diagonal

--- 9 mens morris - oniel.py
+++ 9 mens morris - oniel.py
@@ -57,7 +57,7 @@ def horizontal_moves(board,fil,col):
     move = fil - 3
   else:
     move = 1
-  print('h=',fil,col,move)
+
   if col-move >= 0 and col+move <= 6:
     if board[fil][col-move] == '*' and board[fil][col+move] == '*':
       return True,'='
@@ -81,7 +81,6 @@ def vertical_moves(board,fil,col):
   else:
     move = 1
   
-  print('v=',fil,col,move)
   if fil-move >= 0 and fil+move <= 6:
     if board[fil-move][col] == '*' and board[fil+move][col] == '*':
       return True,'='
@@ -106,10 +105,6 @@ def ficha_move(board,turno,fil,col):
 
 def moves(board,turno,fil,col,direccion_x,direccion_y):
 
-  print ('Valores iniciales')
-
-  print (turno,fil,col,direccion_x,direccion_y)
-
   if fil < 3:
     move_x = (6-fil) - 3
   elif fil > 3:
@@ -124,42 +119,30 @@ def moves(board,turno,fil,col,direccion_x,direccion_y):
   else:
     move_y = 1
 
-  print ('Valores de los moves')
-  print(move_x,move_y)
-
   ficha,turno = tipo_ficha(turno)
-
-  print ('Valor de ficha y turno')
-  print (ficha,turno)
 
   finish = False
 
   if direccion_x == '!':
-    print('solo vertical')
     if direccion_y == '+':
-      print('abajo')
       board[fil][col] = '*'
       fil += move_y 
       board[fil][col] = ficha
       finish = True
     if direccion_y == '-':
-      print('arriba')
       board[fil][col] = '*'
       fil -= move_y 
       board[fil][col] = ficha
       finish = True
     
   if direccion_y == '!':
-    print('solo horizontal')
     if direccion_x == '+':
-      print('derecha')
       board[fil][col] = '*'
       col += move_x 
       print(fil,col)
       board[fil][col] = ficha
       finish = True
     if direccion_x == '-':
-      print('izquierda')
       board[fil][col] = '*'
       col -= move_x 
       board[fil][col] = ficha
@@ -168,7 +151,6 @@ def moves(board,turno,fil,col,direccion_x,direccion_y):
   print('')
 
   while finish == False:
-    print('Dentro del while')
     if direccion_x == '=' and direccion_y == '=':
       ubi = input('Escribe nueva ubicacion (u)(d)(l)(r)')
       if ubi == 'u':
@@ -343,7 +325,6 @@ def moves(board,turno,fil,col,direccion_x,direccion_y):
           board[fil][col] = ficha
           finish = True
 
-
     if finish == False:
       input('algo anda mal')
   return board
@@ -371,44 +352,49 @@ def dezlice(board,turno):
   else: turno = 1
   return board,turno
 
-def molino(board,turno):
-  ficha,turno = tipo_ficha(turno)
-  print('Dentro de molino')
-  for fil in range(0,7):
-    if fil < 3:
-      move = (6-fil) - 3
-      col = fil
-      col_2 = col
-    elif fil > 3:
-      move = fil - 3
-      col = 6-fil
-      col_2 = col
-    else:
-      move = 1
-      col = 0
-      col_2 = 4
+def molino(board):
+  ficha = ['☺','☻']
 
-    if (board[fil][col] == ficha or board[fil][col_2] == ficha) and (board[fil][col+move] == ficha or board[fil][col_2+move] == ficha) and (board[fil][col+(move*2)] == ficha or board[fil][col_2+(move*2)] == ficha):
-      return True
+  for f in ficha:
+    for fil in range(0,7):
+      if fil < 3:
+        move = (6-fil) - 3
+        col = fil
+        col_2 = col
+      elif fil > 3:
+        move = fil - 3
+        col = 6-fil
+        col_2 = col
+      else:
+        move = 1
+        col = 0
+        col_2 = 4
 
-  for col in range(0,7):
-    if col < 3:
-      move = (6-col) - 3
-      fil = col
-      fil_2 = fil
-    elif col > 3:
-      move = col - 3
-      fil = 6-col
-      fil_2 = fil
-    else:
-      move = 1
-      fil = 0
-      fil_2 = 4
+      if board[fil][col] == f or board[fil][col_2] == f:
+        if board[fil][col+move] == f or board[fil][col_2+move] == f:
+          if board[fil][col+(move*2)] == f or board[fil][col_2+(move*2)] == f:
+            return True,f
 
-    if (board[fil][col] == ficha or board[fil][col_2] == ficha) and (board[fil+move][col] == ficha or board[fil+move][col_2] == ficha) and (board[fil+(move*2)][col] == ficha or board[fil+(move*2)][col_2] == ficha):
-      return True
+    for col in range(0,7):
+      if col < 3:
+        move = (6-col) - 3
+        fil = col
+        fil_2 = fil
+      elif col > 3:
+        move = col - 3
+        fil = 6-col
+        fil_2 = fil
+      else:
+        move = 1
+        fil = 0
+        fil_2 = 4
+
+      if board[fil][col] == f or board[fil_2][col] == f:
+        if board[fil+move][col] == f or board[fil_2+move][col] == f:
+          if board[fil+(move*2)][col] == f or board[fil_2+(move*2)][col] == f:
+            return True,f
   
-  return False
+  return False,'!'
 
 def game_loop():
   board = create_board()
@@ -416,15 +402,19 @@ def game_loop():
   etapa = 1
   while turno == 1 or turno == 2:
     print_board(board)
-    if molino(board,turno):
-      print('Hay un molino')
     if etapa == 1:
       print (fichas(board))
       board,turno = goteo(board,turno)
       if fichas(board) == False:
         etapa = 2
-      continue
+        continue
+        
     if etapa == 2:
       board,turno = dezlice(board,turno)
+    mol,f = molino(board)
+    if mol:
+      if f == '☺':
+        print('Hay un molino de ☺')
+      else: print('Hay un molino de ☻')
 
 game_loop()
